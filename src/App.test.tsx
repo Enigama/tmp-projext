18a8e88f6af749586dceaf3d49736f6671c205bd
enigama <enigama3@gmail.com>
2025-03-30 19:51:36 +0300

    test commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
 import { describe, expect, it } from "vitest";
 
 describe("Describe", () => {
-  it("It: ", () => {
-    expect(1).toBe(1);
-    expect(2 + 2).toBe(4);
-    expect(2 + 3).toBe(7);
-  });
+    it("It: ", () => {
+        expect(1).toBe(1);
+        expect(2 + 2).toBe(4);
+    });
 });
